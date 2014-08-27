76ff2f2d5a032bd367529c2a06ca21cc791597b8
Franklin van de Meent <fr@nkl.in>
2014-08-27 03:58:53 +0200

    Convert tabs to spaces

--- centroid.js
+++ centroid.js
@@ -4,12 +4,12 @@ var crypto = require('crypto'),
     util = require('util'),
     querystring = require('querystring'),
     app = {
-	set: {
-		apihost:	'api.centroidmedia.com',
-		apikey:		'',
-		privatekey:	''
-	},
-	currentRate: 0
+  set: {
+    apihost:  'api.centroidmedia.com',
+    apikey:   '',
+    privatekey: ''
+  },
+  currentRate: 0
 }
 
 
@@ -17,188 +17,188 @@ var crypto = require('crypto'),
 app.persons = {}
 
 app.persons.getCurrentRate = function( callback ) {
-	app.talk( 'persons', 'getCurrentRate', function( err, data ) {
-		if( ! err ) {
-			if( data.query === undefined || data.query.currentRate === undefined ) {
-				callback( new Error('Invalid response') )
-			} else {
-				callback( null, data.query.currentRate )
-			}
-		} else {
-			callback( err )
-		}
-	})
+  app.talk( 'persons', 'getCurrentRate', function( err, data ) {
+    if( ! err ) {
+      if( data.query === undefined || data.query.currentRate === undefined ) {
+        callback( new Error('Invalid response') )
+      } else {
+        callback( null, data.query.currentRate )
+      }
+    } else {
+      callback( err )
+    }
+  })
 }
 
 app.persons.getActiveSources = function( params, callback ) {
-	app.talk( 'persons', 'getActiveSources', params, function( err, data ) {
-		if( ! err ) {
-			if( ! util.isArray( data.sources ) ) {
-				callback( new Error('Invalid response') )
-			} else if( data.sources.length === 0 ) {
-				callback( new Error('No results') )
-			} else {
-				callback( null, data.sources )
-			}
-		} else {
-			callback( err )
-		}
-	})
+  app.talk( 'persons', 'getActiveSources', params, function( err, data ) {
+    if( ! err ) {
+      if( ! util.isArray( data.sources ) ) {
+        callback( new Error('Invalid response') )
+      } else if( data.sources.length === 0 ) {
+        callback( new Error('No results') )
+      } else {
+        callback( null, data.sources )
+      }
+    } else {
+      callback( err )
+    }
+  })
 }
 
 app.persons.getPopularSources = function( params, callback ) {
-	app.talk( 'persons', 'getPopularSources', params, function( err, data ) {
-		if( ! err ) {
-			if( ! util.isArray( data.sources ) ) {
-				callback( new Error('Invalid response') )
-			} else if( data.sources.length === 0 ) {
-				callback( new Error('No results') )
-			} else {
-				callback( null, data.sources )
-			}
-		} else {
-			callback( err )
-		}
-	})
+  app.talk( 'persons', 'getPopularSources', params, function( err, data ) {
+    if( ! err ) {
+      if( ! util.isArray( data.sources ) ) {
+        callback( new Error('Invalid response') )
+      } else if( data.sources.length === 0 ) {
+        callback( new Error('No results') )
+      } else {
+        callback( null, data.sources )
+      }
+    } else {
+      callback( err )
+    }
+  })
 }
 
 app.persons.getCategories = function( params, callback ) {
-	app.talk( 'persons', 'getCategories', params, function( err, data ) {
-		if( ! err ) {
-			if( ! util.isArray( data.categories ) ) {
-				callback( new Error('Invalid response') )
-			} else if( data.categories.length === 0 ) {
-				callback( new Error('No results') )
-			} else {
-				callback( null, data.categories )
-			}
-		} else {
-			callback( err )
-		}
-	})
+  app.talk( 'persons', 'getCategories', params, function( err, data ) {
+    if( ! err ) {
+      if( ! util.isArray( data.categories ) ) {
+        callback( new Error('Invalid response') )
+      } else if( data.categories.length === 0 ) {
+        callback( new Error('No results') )
+      } else {
+        callback( null, data.categories )
+      }
+    } else {
+      callback( err )
+    }
+  })
 }
 
 app.persons.search = function( params, callback ) {
-	app.talk( 'persons', 'search', params, function( err, data ) {
-		if( ! err ) {
-			if( ! util.isArray( data.sources ) ) {
-				callback( new Error('Invalid response') )
-			} else if( data.sources.length === 0 ) {
-				callback( new Error('No results') )
-			} else {
-				callback( null, data.sources )
-			}
-		} else {
-			callback( err )
-		}
-	})
+  app.talk( 'persons', 'search', params, function( err, data ) {
+    if( ! err ) {
+      if( ! util.isArray( data.sources ) ) {
+        callback( new Error('Invalid response') )
+      } else if( data.sources.length === 0 ) {
+        callback( new Error('No results') )
+      } else {
+        callback( null, data.sources )
+      }
+    } else {
+      callback( err )
+    }
+  })
 }
 
 
 // ! communicate
 app.talk = function( category, path, params, callback ) {
-	if( typeof params === 'function' ) {
-		var callback = params
-		var params = {}
-	}
-	
-	// check credentials
-	if( typeof app.set.apikey !== 'string' || app.set.apikey === '' ) {
-		callback('No API key')
-		return
-	}
-	
-	if( typeof app.set.privatekey !== 'string' || app.set.privatekey === '' ) {
-		callback('No private key')
-		return
-	}
-	
-	// sign
-	var signature = Date.now()
-	var md5 = crypto.createHash('md5')
-	md5.update( app.set.privatekey + signature )
-	var api_sig = md5.digest('hex')
-	
-	// build request
-	params.api_key = app.set.apikey
-	params.api_sig = api_sig
-	params.signature = signature
-	params.format = 'json'
-	
-	var options = {
-		host: category +'.'+ app.set.apihost,
-		path: '/'+ path +'?'+ querystring.stringify( params ),
-		method: 'GET'
-	}
-	
-	var request = http.request( options )
-	
-	// process response
-	request.on( 'response', function( response ) {
-		var data = ''
-		var complete = false
-		
-		response.on( 'data', function( ch ) { data += ch })
-		
-		response.on( 'close', function() {
-			if( ! complete ) {
-				complete = true
-				callback( new Error('Disconnected') )
-			}
-		})
-		
-		response.on( 'end', function() {
-			if( ! complete ) {
-				complete = true
-				data = data.toString('utf8').trim()
-				var err = null
-				
-				if( response.statusCode >= 300 ) {
-					var err = new Error('HTTP error')
-				} else if( data == '' ) {
-					var err = new Error('No response')
-				} else if( ! data.match( /^\{.*\}$/ ) ) {
-					var err = new Error('Invalid response')
-				} else {
-					// parse JSON
-					data = JSON.parse( data )
-					
-					// API error
-					if( data.errorCode !== undefined ) {
-						var err = new Error('API error')
-						err.errorCode = data.errorCode
-						err.errorString = data.errorString
-					}
-					
-					// store rate limit
-					if( data.query !== undefined && data.query.currentRate !== undefined ) {
-						app.currentRate = data.query.currentRate
-					}
-				}
-				
-				if( err instanceof Error ) {
-					err.httpCode = response.statusCode
-					err.httpHeaders = response.headers
-					err.request = options
-					err.response = data
-					callback( err )
-				} else {
-					callback( null, data )
-				}
-			}
-		})
-	})
-	
-	// request failed
-	request.on( 'error', function( error ) {
-		var err = new Error('Request failed')
-		err.request = options
-		err.requestError = error
-		callback( err )
-	})
-	
-	// finish
-	request.end()
+  if( typeof params === 'function' ) {
+    var callback = params
+    var params = {}
+  }
+  
+  // check credentials
+  if( typeof app.set.apikey !== 'string' || app.set.apikey === '' ) {
+    callback('No API key')
+    return
+  }
+  
+  if( typeof app.set.privatekey !== 'string' || app.set.privatekey === '' ) {
+    callback('No private key')
+    return
+  }
+  
+  // sign
+  var signature = Date.now()
+  var md5 = crypto.createHash('md5')
+  md5.update( app.set.privatekey + signature )
+  var api_sig = md5.digest('hex')
+  
+  // build request
+  params.api_key = app.set.apikey
+  params.api_sig = api_sig
+  params.signature = signature
+  params.format = 'json'
+  
+  var options = {
+    host: category +'.'+ app.set.apihost,
+    path: '/'+ path +'?'+ querystring.stringify( params ),
+    method: 'GET'
+  }
+  
+  var request = http.request( options )
+  
+  // process response
+  request.on( 'response', function( response ) {
+    var data = ''
+    var complete = false
+    
+    response.on( 'data', function( ch ) { data += ch })
+    
+    response.on( 'close', function() {
+      if( ! complete ) {
+        complete = true
+        callback( new Error('Disconnected') )
+      }
+    })
+    
+    response.on( 'end', function() {
+      if( ! complete ) {
+        complete = true
+        data = data.toString('utf8').trim()
+        var err = null
+        
+        if( response.statusCode >= 300 ) {
+          var err = new Error('HTTP error')
+        } else if( data == '' ) {
+          var err = new Error('No response')
+        } else if( ! data.match( /^\{.*\}$/ ) ) {
+          var err = new Error('Invalid response')
+        } else {
+          // parse JSON
+          data = JSON.parse( data )
+          
+          // API error
+          if( data.errorCode !== undefined ) {
+            var err = new Error('API error')
+            err.errorCode = data.errorCode
+            err.errorString = data.errorString
+          }
+          
+          // store rate limit
+          if( data.query !== undefined && data.query.currentRate !== undefined ) {
+            app.currentRate = data.query.currentRate
+          }
+        }
+        
+        if( err instanceof Error ) {
+          err.httpCode = response.statusCode
+          err.httpHeaders = response.headers
+          err.request = options
+          err.response = data
+          callback( err )
+        } else {
+          callback( null, data )
+        }
+      }
+    })
+  })
+  
+  // request failed
+  request.on( 'error', function( error ) {
+    var err = new Error('Request failed')
+    err.request = options
+    err.requestError = error
+    callback( err )
+  })
+  
+  // finish
+  request.end()
 }
 
 
